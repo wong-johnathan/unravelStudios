30ea399b42f170e87fe7816adf2bd885fe17566d
wong-johnathan <wong.johnathanwh@gmail.com>
2022-09-07 12:26:32 +0800

    Remove node-sass

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import reducer from "./store/reducers";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "styles/index.scss";
+// import "styles/index.scss";
 
 const store = createStore(reducer, applyMiddleware(thunk));
 
